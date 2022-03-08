a82552393684eb4e842e16c202c90b3c2a317024
ccql <chaoql@126.com>
2022-03-08 16:28:02 +0800

    模糊查询功能完成

--- controllers/index.py
+++ controllers/index.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, redirect
 from application import db
 from common.models.news import News
-from common.libs.FLHelper.Helper import ops_render, iPageNation
+from common.libs.FLHelper.Helper import ops_render, iPageNation, ops_renderJSON
 from common.libs.FLHelper.UrlManager import UrlManager
 from sqlalchemy.sql.expression import func
 from application import app
@@ -19,3 +19,20 @@ def index():
 @index_page.route("/single")
 def sigal():
     return ops_render("single.html")
+
+
+@index_page.route("/search", methods=["POST", "GET"])
+def search():
+    app.logger.warning("==============search=============")
+    if request.method == "GET":
+        return ops_render("search.html")
+    req = request.form
+    app.logger.warning(req)
+    model_news = News.query.filter(News.text.like("%" + req['search_str'] + "%")).all()
+    app.logger.warning(req)
+    app.logger.warning(model_news)
+    count = len(model_news)
+    app.logger.warning(count)
+    app.logger.warning("---------------------------------")
+    return ops_render("search.html", {'newsl': model_news, 'count': count,
+                                      "pic_path": app.config['DOMAIN']['www'] + "static/images/news/"})
